9f0093dd5c1b7043e27e44b6c036190822f287e7
youssefg7 <67619125+youssefg7@users.noreply.github.com>
2022-10-26 18:44:51 +0200

    aws rds connected

--- agileProject/settings.py
+++ agileProject/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +79,12 @@ WSGI_APPLICATION = "agileProject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "churchdb",
+        "USER": "admin",
+        "PASSWORD": "YGF0000ygfssms",
+        "HOST": "agileproject.csyseqszqjec.us-east-1.rds.amazonaws.com",
+        "PORT": 3306
     }
 }
 
