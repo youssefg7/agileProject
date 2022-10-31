c2b014c3721f5be7699584b55a312bc9f58478f4
Anthony-Amgad <19p9880@eng.asu.edu.eg>
2022-10-31 12:11:19 +0200

    login fix

--- loginApp/views.py
+++ loginApp/views.py
@@ -18,7 +18,7 @@ def donorLoginSubmit(request):
         return HttpResponseRedirect(reverse('donorLoginPage'))
     else:
         if fquery.all().values()[0]["password"] == fpassword:
-                response = HttpResponseRedirect(reverse('index'))
+                response = HttpResponseRedirect(reverse('donorMyaccount'))
                 max_age = 365 * 24 * 60 * 60
                 expires = datetime.datetime.strftime(datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age),"%a, %d-%b-%Y %H:%M:%S GMT",)
                 response.set_cookie("userid", fquery.all().values()[0]["user_id"], max_age=max_age,expires=expires,)
