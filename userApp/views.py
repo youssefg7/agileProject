53fc50c51e1863b470eb781072226815d621bf5b
Anthony-Amgad <19p9880@eng.asu.edu.eg>
2022-12-19 15:55:18 +0200

    begining homepage

--- userApp/views.py
+++ userApp/views.py
@@ -8,7 +8,8 @@ def index(request):
         rolenum = models.User.objects.get(user_id = request.COOKIES['userid']).role
         role = models.Roles.objects.get(role_number = rolenum).role_name
         name = models.User.objects.get(user_id = request.COOKIES['userid']).name
-        dict = {'role' : role, 'name': name, 'rolenum': rolenum}
+        array = [1,2]
+        dict = {'role' : role, 'name': name, 'rolenum': rolenum, 'array':array}
     except:
         dict = {'role' : "Anon", 'name': "", 'rolenum': -1}
     
