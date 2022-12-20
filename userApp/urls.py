7330a612a8369c55a9963e36f1a402d46ddbe4e1
Kerollos <kerolloswageeh03@gmail.com>
2022-12-20 21:42:44 +0200

    commit

--- userApp/urls.py
+++ userApp/urls.py
@@ -8,11 +8,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("about/", views.aboutPage, name="about"),
     path('creditCardDonation/', views.creditCardDonation, name='creditCardDonation'),
-<<<<<<< HEAD
     path('inPersonDonation/', views.inPersonDonation, name='inPersonDonation'),
-=======
     path('adminInv/', views.adminInvPage, name='adminInvPage'),
     path('adminPeopleIN/', views.adminPeopleINPage, name='adminPeopleIN'),
 
->>>>>>> 2bcd48137d6921e30e19b647fe012f9c5953ccd2
 ]
